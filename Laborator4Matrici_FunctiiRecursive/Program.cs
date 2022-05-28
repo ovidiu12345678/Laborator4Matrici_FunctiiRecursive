6b19499d41c6c4975f6e77c3806013e7b5e69736
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-05-28 17:39:30 +0300

    Add project files.

---
+++ Laborator4Matrici_FunctiiRecursive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laborator4Matrici_FunctiiRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
